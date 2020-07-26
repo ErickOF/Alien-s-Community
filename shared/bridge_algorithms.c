0c9e2d2aeb305ffbe501ceb27d40169d7c7bed78
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-07-25 19:36:36 -0600

    [Update] - Y Algorithm

--- shared/bridge_algorithms.c
+++ shared/bridge_algorithms.c
@@ -22,32 +22,34 @@ void y_algorithm(Bridge bridge) {
             }
         }
 
-        if (bridge.south_aliens_number > bridge.north_aliens_number){
-            bridge.direction = 1;
-            for (int i=bridge.south_aliens_number; i>0; i--){
-                bridge.south_aliens_number -= 1;
-                printf("(mayor q norte)Aliens al sur: %d \n", bridge.south_aliens_number);
-            }
-        }
-
-        if (bridge.north_aliens_number > bridge.south_aliens_number){
-            bridge.direction = 0;
-            for (int i=bridge.north_aliens_number; i>0; i--){
-                bridge.north_aliens_number -= 1;
-                printf("(mayor q sur)Aliens al norte: %d \n", bridge.north_aliens_number);
-            }
-        }
-        if (bridge.south_aliens_number == bridge.north_aliens_number){
-            if (bridge.direction == 0){
-                for (int i=bridge.north_aliens_number; i>0; i--){
-                bridge.north_aliens_number -= 1;
-                printf("(dir)Aliens al norte: %d \n", bridge.north_aliens_number);
+        else{
+            if (bridge.south_aliens_number > bridge.north_aliens_number){
+                bridge.direction = 1;
+                for (int i=bridge.south_aliens_number; i>0; i--){
+                    bridge.south_aliens_number -= 1;
+                    printf("(mayor q norte)Aliens al sur: %d \n", bridge.south_aliens_number);
                 }
             }
-            else {
-                for (int j=bridge.south_aliens_number; j>0; j--){
-                bridge.south_aliens_number -= 1;
-                printf("(dir)Aliens al sur: %d \n", bridge.north_aliens_number);
+
+            if (bridge.north_aliens_number > bridge.south_aliens_number){
+                bridge.direction = 0;
+                for (int i=bridge.north_aliens_number; i>0; i--){
+                    bridge.north_aliens_number -= 1;
+                    printf("(mayor q sur)Aliens al norte: %d \n", bridge.north_aliens_number);
+                }
+            }
+            if (bridge.south_aliens_number == bridge.north_aliens_number){
+                if (bridge.direction == 0){
+                    for (int i=bridge.north_aliens_number; i>0; i--){
+                    bridge.north_aliens_number -= 1;
+                    printf("(dir)Aliens al norte: %d \n", bridge.north_aliens_number);
+                    }
+                }
+                else {
+                    for (int j=bridge.south_aliens_number; j>0; j--){
+                    bridge.south_aliens_number -= 1;
+                    printf("(dir)Aliens al sur: %d \n", bridge.north_aliens_number);
+                    }
                 }
             }
         }
