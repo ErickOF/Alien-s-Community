459b38c6cb5a9ea92330b397b4326bbd912632da
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2020-07-26 21:09:54 -0600

    [Fix] - Bridge compilation with MUTEX

--- bridge_test.c
+++ bridge_test.c
@@ -2,55 +2,53 @@
 #include <stdlib.h>
 
 int main() {
-    Bridge bridge;
-    bridge.y = 2;
-    bridge.direction = 0;
-    bridge.south_aliens_number = 6;
-    bridge.north_aliens_number = 5;
-    bridge.max_south_aliens = 10;
-    bridge.max_north_aliens = 10;
-    bridge.max_weight = 5;
-    bridge.north_waiting_seconds = 2.4;
-    bridge.south_waiting_seconds = 2.4;
+    Bridge bridge = {
+        .y = 2, .direction = 0,
+        .south_aliens_number = 6,
+        .north_aliens_number = 5,
+        .max_south_aliens = 10,
+        .max_north_aliens = 10,
+        .max_weight = 5,
+        .north_waiting_seconds = 2.4,
+        .south_waiting_seconds = 2.4
+    };
+
+    Lmutex_init(&bridge.south_mutex, NULL);
+    Lmutex_init(&bridge.north_mutex, NULL);
 
     bridge.south_aliens = (Alien*) malloc(sizeof(Alien) * bridge.max_south_aliens);
     bridge.north_aliens = (Alien*) malloc(sizeof(Alien) * bridge.max_north_aliens);
     int max_number = bridge.south_aliens_number+bridge.north_aliens_number;
 
     for (int i = 0; i < bridge.north_aliens_number - 1; i++) {
-        Alien alien;
-        alien.status = 3;
-        alien.weight = 2.1;
-        bridge.north_aliens[i] = alien;
+        bridge.north_aliens[i].status = 3;
+        bridge.north_aliens[i].weight = 2.1;
     }
 
     for (int i = 0; i < bridge.south_aliens_number; i++) {
-        Alien alien;
-        alien.status = 3;
-        alien.weight = 2.1;
-        bridge.south_aliens[i] = alien;
+        bridge.north_aliens[i].status = 3;
+        bridge.north_aliens[i].weight = 2.1;
     }
 
     for (int i = 0; i < bridge.south_aliens_number; i = i+2) {
-        Alien alien;
-        alien.status = 2;
-        alien.weight = 2.1;
-        bridge.south_aliens[i] = alien;
+        bridge.north_aliens[i].status = 2;
+        bridge.north_aliens[i].weight = 2.1;
     }
 
     for (int i = 0; i < bridge.north_aliens_number; i = i+2) {
-        Alien alien;
-        alien.status = 2;
-        alien.weight = 2.1;
-        bridge.north_aliens[i] = alien;
+        bridge.north_aliens[i].status = 2;
+        bridge.north_aliens[i].weight = 2.1;
     }
 
     //*y_algorithm(&bridge);*//
-    //*survival_algorithm(&bridge);*//
+    //survival_algorithm(&bridge);
     sem_algorithm(&bridge);
 
     free(bridge.north_aliens);
     free(bridge.south_aliens);
+
+    Lmutex_destroy(bridge.south_mutex);
+    Lmutex_destroy(bridge.north_mutex);
 
     return 0;
 }
